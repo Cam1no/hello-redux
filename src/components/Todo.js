c22f98c177ae3706fbd4a44e4105fb3a120a6898
kinjo yuji <kinjoyuji@gmail.com>
2017-10-20 01:54:18 +0900

    complateフラグ実装

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Todo = ({ text }) => (
-  <li>
+const Todo = ({ completed, text }) => (
+  <li style={{textDecoration: completed ? 'line-through' : 'none'}}>
     {text}
   </li>
 )
 
 Todo.propTypes = {
+  completed: PropTypes.bool.isRequired,
   text: PropTypes.string.isRequired
 }
 
--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import todo from './reducers'
 import App from './components/App'
-import { addTodo } from './actions'
+import { addTodo, toggleTodo } from './actions'
 
 let store = createStore(todo)
 
 store.dispatch(addTodo('Hello React!'))
 store.dispatch(addTodo('Hello Redux!'))
+store.dispatch(addTodo('Hello React!'))
+store.dispatch(toggleTodo(0))
 
 console.log(store.getState())
 
