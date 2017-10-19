e3a08f9e8924de4997310dfe1a8dfa8d688cdf3b
kinjo yuji <kinjoyuji@gmail.com>
2017-10-20 02:06:27 +0900

    todoをクリックするとcomplateフラグが立つように実装

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Todo = ({ completed, text }) => (
-  <li style={{textDecoration: completed ? 'line-through' : 'none'}}>
+const Todo = ({ onClick, completed, text }) => (
+  <li
+    style={{textDecoration: completed ? 'line-through' : 'none'}}
+    onClick={onClick}
+  >
     {text}
   </li>
 )
 
 Todo.propTypes = {
+  onClick: PropTypes.func.isRequired,
   completed: PropTypes.bool.isRequired,
   text: PropTypes.string.isRequired
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,12 +2,13 @@ import React from 'react'
 import Todo from './Todo'
 import PropTypes from 'prop-types'
 
-const TodoList = ({ todos }) => (
+const TodoList = ({ todos, onTodoClick }) => (
   <ul>
     {todos.map((todo) =>
       <Todo
         key={todo.id}
         {...todo}
+        onClick={() => onTodoClick(todo.id)}
       />
     )}
   </ul>
@@ -17,7 +18,8 @@ TodoList.propTypes = {
   todos: PropTypes.arrayOf(PropTypes.shape({
     id: PropTypes.number.isRequired,
     text: PropTypes.string.isRequired
-  }).isRequired).isRequired
+  }).isRequired).isRequired,
+  onTodoClick: PropTypes.func.isRequired
 }
 
 export default TodoList
