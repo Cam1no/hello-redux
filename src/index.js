f633049f5accaac69f0161030ef1fe0e758c4caa
kinjo yuji <kinjoyuji@gmail.com>
2017-10-21 00:17:48 +0900

    action/reducerを追加してdevツールで確認

--- src/index.js
+++ src/index.js
@@ -5,9 +5,13 @@ import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import todo from './reducers'
 import App from './components/App'
-import { addTodo, toggleTodo } from './actions'
+import { addTodo, toggleTodo, setVisibilityFilter } from './actions'
 
 let store = createStore(todo)
+
+console.log(store.getState()) // => Object {todos: Array[0], visibilityFilter: "SHOW_ALL"}
+store.dispatch(setVisibilityFilter('SHOW_COMPLETED'))
+console.log(store.getState()) // => Object {todos: Array[0], visibilityFilter: "SHOW_COMPLETED"}
 
 store.dispatch(addTodo('Hello React!'))
 store.dispatch(addTodo('Hello Redux!'))
